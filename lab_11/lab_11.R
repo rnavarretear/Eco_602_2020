9338fa63dfcc08fd7c8c085e543a04f01665eedd
rnavarretear <70414131+rnavarretear@users.noreply.github.com>
2020-12-04 12:21:49 -0500

    Changes

--- lab_11/lab_11.R
+++ lab_11/lab_11.R
@@ -301,29 +301,31 @@ load(file = here::here("data", "lab_11_n_effect_sizes.Rdata")))
 #First question
 
 alpha = 0.05
-n_sims = 10
+n_sims = 500
 p_vals = numeric(n_sims)
 
-n_sds = 20
+n_sds = 500
 pop_sds = seq(from = 0.01, to = 1.5, length.out = n_sds)
 
 pop_sd_power = numeric(n_sds)
 
 for(j in 1:length(pop_sds))
 {
-  pop_sd_j = pop_sds
+  pop_sd_j = pop_sds[j]
   for(i in 1:n_sims)
   {
     fit_sim = linear_sim_fit(
       x = birdhab$ls,
       y_int = int_obs,
-      slope = pop_sd_j,
-      st_dev = sd_obs
+      slope = slope_obs,
+      st_dev = pop_sd_j
     )
     p_vals[i] = summary(fit_sim)$coefficients[2, 'Pr(>|t|)']
   }
   pop_sd_power[j] = sum(p_vals < alpha) / n_sims
 }
+
+
 
 sim_output_dispersion = data.frame(
   sd = pop_sds,
@@ -332,25 +334,18 @@ sim_output_dispersion = data.frame(
 
 
 plot(
-  power ~ sd, data = sim_output_3,
+  power ~ sd, data = sim_output_dispersion,
   type = 'l', xlab = 'Standard Deviation', ylab = 'Power')
 abline(v = coef(fit_1)[2], lty = 2, col = 'red')
 
 
 #Second and third question
+
 alpha = 0.05
 n_sims = 100
 p_vals = numeric(n_sims)
 
-# What was the observed standard deviation?
-sd_obs
-
-# specify the number of different standard deviation values to simulate:
-alpha = 0.05
-n_sims = 100
-p_vals = numeric(n_sims)
-
-n_sds = 50
+n_sds = 100
 pop_sds = seq(from = 0.05, to = 1.5, length.out = n_sds)
 
 pop_sd_power = numeric(n_sds)
